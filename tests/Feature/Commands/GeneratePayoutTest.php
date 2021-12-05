9e46e185c2eaa7e81ac41c5b529e6b4b2ea5f103
Fanan Dala <fanan.dala@yahoo.com>
2021-12-05 05:50:46 +0100

    tests: fix failing tests

--- tests/Feature/Commands/GeneratePayoutTest.php
+++ tests/Feature/Commands/GeneratePayoutTest.php
@@ -18,8 +18,8 @@ class GeneratePayoutTest extends TestCase
     {
         $user1 = User::factory()->create();
         $user1_number_of_sales = 13;
-        $user1_unit_price = $this->faker->randomFloat(2, 5, 100);
-        $user1_total = bcmul($user1_unit_price, $user1_number_of_sales, 6);
+        $user1_unit_price = $this->faker->randomFloat(0, 5, 100);
+        $user1_total = bcmul($user1_unit_price, $user1_number_of_sales, 2);
         $user1_final_share = bcmul($user1_total, Constants::CREATOR_SHARE, 6);
         
         Sale::factory()
@@ -30,7 +30,7 @@ class GeneratePayoutTest extends TestCase
 
         $user2 = User::factory()->create();
         $user2_number_of_sales = 6;
-        $user2_unit_price = $this->faker->randomFloat(2, 5, 100);
+        $user2_unit_price = $this->faker->randomFloat(0, 5, 100);
         $user2_total = ($user2_unit_price * $user2_number_of_sales);
         $user2_final_share = bcmul($user2_total, Constants::CREATOR_SHARE, 6);
         Sale::factory()
@@ -47,7 +47,7 @@ class GeneratePayoutTest extends TestCase
 
         $user3 = User::factory()->create();
         $user3_number_of_sales = 8;
-        $user3_unit_price = $this->faker->randomFloat(2, 5, 100);
+        $user3_unit_price = $this->faker->randomFloat(0, 5, 100);
         $user3_total = 0;
         $user3_final_share = 0;
         Sale::factory()
