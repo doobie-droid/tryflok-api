76fee1cccf0d88e941bf6fef52ba7f4089552834
Fanan Dala <fanan.dala@yahoo.com>
2021-12-08 17:35:09 +0100

    fix flaky username generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'id' => $this->faker->unique()->uuid,
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'username' => $this->faker->username . date('YmdHis'),
+            'username' => $this->faker->unique()->username . date('YmdHis'),
             'bio' => $this->faker->sentence(20),
             'email_verified' => 1,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
