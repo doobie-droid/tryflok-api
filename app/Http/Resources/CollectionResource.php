55d213909b374d3fd7b38abbe44af1125a452f35
Fanan Dala <fanan.dala@yahoo.com>
2021-12-25 22:16:52 +0100

    chore: lint codebase

--- app/Http/Resources/CollectionResource.php
+++ app/Http/Resources/CollectionResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Resources\UserResource;
 use App\Http\Resources\UserableResource;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CollectionResource extends JsonResource
