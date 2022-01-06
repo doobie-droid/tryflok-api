ee759b00c0d53bdf8c9a3794432ef14bccf6540b
Fanan Dala <fanan.dala@yahoo.com>
2022-01-06 13:12:16 +0100

    feat: queues should run after commits

--- config/queue.php
+++ config/queue.php
@@ -68,7 +68,7 @@ return [
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 7200,
             'block_for' => null,
-            'after_commit' => false,
+            'after_commit' => true,
         ],
 
     ],
