648e34f19162078603f0a9c2d5e6130fa3815296
Fanan Dala <fanan.dala@yahoo.com>
2022-03-28 00:29:34 +0100

    chore: lint codebase

--- database/migrations/2022_03_27_211033_drop_agora_uid_from_content_challenge_contestants_table.php
+++ database/migrations/2022_03_27_211033_drop_agora_uid_from_content_challenge_contestants_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class DropAgoraUidFromContentChallengeContestantsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('content_challenge_contestants', function (Blueprint $table) {
@@ -18,11 +13,6 @@ class DropAgoraUidFromContentChallengeContestantsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('content_challenge_contestants', function (Blueprint $table) {
--- database/migrations/2022_03_27_211253_create_content_live_broadcasters_table.php
+++ database/migrations/2022_03_27_211253_create_content_live_broadcasters_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateContentLiveBroadcastersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('content_live_broadcasters', function (Blueprint $table) {
@@ -24,11 +19,6 @@ class CreateContentLiveBroadcastersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('content_live_broadcasters');
