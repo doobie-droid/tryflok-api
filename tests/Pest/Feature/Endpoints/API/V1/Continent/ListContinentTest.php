af87901be49d0a07ffd1075021d31717051c650b
charley.com <charlesagate3@gmail.com>
2022-06-26 16:19:44 +0100

    refactor: completed ListContinentTest in pest

---
+++ tests/Pest/Feature/Endpoints/API/V1/Continent/ListContinentTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+test('list continent works', function()
+{
+    $response = $this->json('GET', '/api/v1/continents');
+        $response->assertStatus(200)->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                'continents' => [
+                    [
+                        'id',
+                        'name',
+                        'iso_code',
+                    ],
+                ],
+            ],
+        ]);
+});
