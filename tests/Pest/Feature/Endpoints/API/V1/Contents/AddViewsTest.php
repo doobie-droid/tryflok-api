abb12373c152090df762a08b05e4182c2861f20a
charley.com <charlesagate3@gmail.com>
2022-07-08 18:26:18 +0100

    test: update add views test

--- tests/Pest/Feature/Endpoints/API/V1/Contents/AddViewsTest.php
+++ tests/Pest/Feature/Endpoints/API/V1/Contents/AddViewsTest.php
@@ -1,1 +1,30 @@
 <?php 
+
+use App\Models;
+use Tests\MockData;
+
+test('add views works', function()
+{
+        $user = Models\User::factory()->create();
+        $this->be($user);
+        $content = Models\Content::factory()
+        ->create();
+
+        $request = MockData\View::generateStandardAddViewRequest();
+        $expected_response_structure = MockData\View::generateAddViewResponse();
+        $response = $this->json('POST', '/api/v1/contents/{$content->id}/views', $request);
+        $response->assertStatus(200)
+        ->assertJsonStructure($expected_response_structure);
+
+        $this->assertDatabaseHas('views', [
+            'id' => $request['id'],
+            'user_id' => $user->id,
+            'viewable_type' => 'content',
+            'viewable_id' => $content->id,
+
+        ]);
+});
+
+
+
+
