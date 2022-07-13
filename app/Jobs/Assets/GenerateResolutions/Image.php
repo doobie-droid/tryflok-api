52ab0003c9177400cefa0cd98b05c90728cce282
Fanan Dala <fanan.dala@yahoo.com>
2022-07-13 05:17:27 +0100

    fix: image class issue

--- app/Jobs/Assets/GenerateResolutions/Image.php
+++ app/Jobs/Assets/GenerateResolutions/Image.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use \Image as ImageManipulator;
+use Intervention\Image\Facades\Image as ImageManipulator;
 
 class Image implements ShouldQueue
 {
