e676b1b19709a86f02007fd159b7155369dcc486
Fanan Dala <fanan.dala@yahoo.com>
2022-07-13 05:22:52 +0100

    feat: increase image dimensions

--- app/Jobs/Assets/GenerateResolutions/Image.php
+++ app/Jobs/Assets/GenerateResolutions/Image.php
@@ -81,7 +81,7 @@ class Image implements ShouldQueue
     {
         $image = ImageManipulator::make($source);
         $image->orientate();
-        $image->resize(350, 350, function ($constraint) {
+        $image->resize(500, 500, function ($constraint) {
             $constraint->aspectRatio();
             $constraint->upsize();
         });
