6e57a4a1ff0da6f0d9d5c43865f569d5b929a882
charley.com <charlesagate3@gmail.com>
2022-07-15 22:01:29 +0100

    test: create test for list collection reviews endpoint

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'V1'], function () {
     Route::group(['prefix' => 'collections'], function () {
         Route::get('{id}', 'CollectionController@showCollection')->name('show-collection');
         Route::get('{collection_id}/contents', 'ContentController@getCollectionContents');
-        Route::get('{id}/reviews', 'CollectionController@getReviews');
+        Route::get('{id}/reviews', 'CollectionController@listReviews')->name('list-collection-reviews');
     });
 
     Route::group(['prefix' => 'reviews'], function () {
