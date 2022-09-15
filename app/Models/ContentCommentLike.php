0a7c26aec0468118ca2aaff1f8922315a0e168bd
charley.com <charlesagate3@gmail.com>
2022-09-15 21:26:02 +0100

    feat: created content comment like migration

---
+++ app/Models/ContentCommentLike.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContentCommentLike extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_09_15_211438_create_content_comment_likes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentCommentLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_comment_likes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_comment_likes');
+    }
+}
