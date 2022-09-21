739e3d2983a5c43731086a9a7accbc8fc8aae594
charley.com <charlesagate3@gmail.com>
2022-09-22 00:08:51 +0100

    feat: return subscriptionable with owner

---
+++ tests/Pest/Feature/Endpoints/API/V1/Subscriptions/GetUserSubscriptionTest.php
@@ -0,0 +1,34 @@
+<?php 
+
+use App\Models;
+
+beforeEach(function()
+{
+        $this->user = Models\User::factory()->create();
+        $this->collection = Models\Collection::factory()
+        ->digiverse()
+        ->create();
+        $this->price = $this->collection->prices()->first();
+});
+
+test('toggle auto renew works', function()
+{
+        $userable = Models\Userable::factory()
+        ->for($this->user)
+        ->state([
+            'userable_type' => 'collection',
+            'userable_id' => $this->collection->id,
+        ])
+        ->create();
+        $subscription = Models\Subscription::factory()
+        ->for($userable)
+        ->for($this->collection, 'subscriptionable')
+        ->for($this->price)
+        ->create();
+
+        $this->be($this->user);
+
+        $response = $this->json('GET', "/api/v1/account/subscriptions");
+        dd($response->getData());
+        $response->assertStatus(200);
+});
