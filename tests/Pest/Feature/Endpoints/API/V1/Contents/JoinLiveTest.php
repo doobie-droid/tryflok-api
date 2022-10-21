cb32890a4c56622b6bd9ce139c9106ee31012c60
charley.com <charlesagate3@gmail.com>
2022-10-21 07:07:29 +0100

    test: skip join live for anonymous user

--- tests/Pest/Feature/Endpoints/API/V1/Contents/JoinLiveTest.php
+++ tests/Pest/Feature/Endpoints/API/V1/Contents/JoinLiveTest.php
@@ -1,6 +1,8 @@
 <?php 
 
 use App\Models;
+use App\Services\LiveStream\Agora\RtcTokenBuilder as AgoraRtcToken;
+use App\Services\LiveStream\Agora\RtmTokenBuilder as AgoraRtmToken;
 
 it('fails if channel not started', function()
 {
@@ -57,14 +59,44 @@ test('join live works for anonymous user', function ()
             'anonymous_purchaseable_id' => $content->id,
             'status' => 'available'
         ]);
-        $this->be($user2);
-        $this->json('POST', "/api/v1/contents/{$content->id}/live");
+        $channel = $content->metas()->create([
+            'key' => 'channel_name',
+            'value' => "{$content->id}-" . date('Ymd'),
+        ]);
+        $expires = time() + (24 * 60 * 60); // let token last for 24hrs
+        $agora_rtc_token = AgoraRtcToken::buildTokenWithUid(config('services.agora.id'), config('services.agora.certificate'), $channel->value, 0, AgoraRtcToken::ROLE_PUBLISHER, $expires);
+        $agora_rtm_token = AgoraRtmToken::buildToken(config('services.agora.id'), config('services.agora.certificate'), $channel->value, 0, AgoraRtmToken::ROLE_RTM_USER, $expires);
 
+        
+        $content->metas()->createMany([
+            [
+                'key' => 'channel_name',
+                'value' => "{$content->id}-" . date('Ymd'),
+            ],
+            [
+                'key' => 'rtm_token',
+                'value' => $agora_rtm_token,
+            ],
+            [
+                'key' => 'join_count',
+                'value' => 1,
+            ],
+        ]);
+        $content->metas()->create([
+                'key' => 'rtc_token',
+                'value' => $agora_rtc_token,
+        ]);
+
+        $content->live_status = 'active';
+        $content->scheduled_date = now();
+        $content->save();
+        
         $request = [
             'access_token' => $accessToken,
         ];
 
         $response = $this->json('PATCH', "/api/v1/contents/{$content->id}/live", $request);
+        // dd($response->getData());
         $response->assertStatus(200)
         ->assertJsonStructure([
             'data' => [
@@ -74,5 +106,9 @@ test('join live works for anonymous user', function ()
                 'uid',
                 'subscribers_count',
             ],
+        ]);
+        $this->assertDatabaseHas('content_subscriber', [
+            'content_id' => $content->id,
+            'access_token' => $request['access_token']
         ]);
 })->skip();
