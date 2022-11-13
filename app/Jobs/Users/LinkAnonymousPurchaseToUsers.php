30e6f91cccef1c17dfbb6e40bed969fb009901b2
charley.com <charlesagate3@gmail.com>
2022-11-13 05:36:27 +0100

    feat: change logging for linking

--- app/Jobs/Users/LinkAnonymousPurchaseToUsers.php
+++ app/Jobs/Users/LinkAnonymousPurchaseToUsers.php
@@ -34,10 +34,10 @@ class LinkAnonymousPurchaseToUsers implements ShouldQueue
     public function handle()
     {
         try {
-            Log::info("Begin Linking Anonymous Purchases To Users");
             $anonymous_email = $this->anonymous_purchase->email;
             $user = Models\User::where('email', $anonymous_email)->first();
             if (! is_null($user)) {
+                Log::info("Begin Linking Anonymous Purchases To Users");
                 Models\Userable::create([
                     'user_id' => $user->id,
                     'status' => 'available',
@@ -47,8 +47,8 @@ class LinkAnonymousPurchaseToUsers implements ShouldQueue
 
                 $this->anonymous_purchase->link_user_id = $user->id;
                 $this->anonymous_purchase->save();
+                Log::info("End Linking Anonymous Purchases To Users");
             }
-            Log::info("End Linking Anonymous Purchases To Users");
         } catch (\Exception $exception) {
             throw $exception;
             Log::error($exception);
